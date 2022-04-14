764a1e99280a6abd51ab9613c2c173384c31ce3f
Jacqueline Wang'ombe <94549069+Jwangombe11@users.noreply.github.com>
2022-04-14 20:13:56 +0300

    Update clt_demo.py

--- clt_demo.py
+++ clt_demo.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import re
-import sys
-import sys
-from streamlit import cli as stcli
-
-if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "clt_demo.py"]
-    sys.exit(stcli.main())
-
 st.write('Hello World')
 
 streamlit run clt_demo.py 
