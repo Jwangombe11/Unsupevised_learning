7973000dba292cb20d719491e0eaf52426454804
Jacqueline Wang'ombe <94549069+Jwangombe11@users.noreply.github.com>
2022-04-14 21:24:03 +0300

    Add files via upload

--- clt_demo2.py
+++ clt_demo2.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 perc_heads = st.number_input(label = 'Chance of Coins Landing on Heads', min_value = 0.0, max_value = 1.0, value = .5)
+graph_title = st.text_input(label='Graph Title')
 binom_dist = np.random.binomial(1, perc_heads, 1000)
 
 list_of_means = []
